21922451aaeecb24ecc4c8458b822ec62a1c12aa
Danny Vega <67448064+dannyvega-studio@users.noreply.github.com>
2021-07-20 08:56:09 -0600

    add dynamic GET route

--- server.js
+++ server.js
@@ -15,5 +15,16 @@ app.get('/users', function(req,res){
  	})
 })
 
+// Access to values of each variable //
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+
 // Server Conection //
 app.listen(8000,function(){console.log("server is running")})
